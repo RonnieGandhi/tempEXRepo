7f5aeca3349d712e8afbc0cc0a5638fea0566c2d
Ronnie Gandhi <ronniegandhi19999@gmail.com>
2021-09-07 16:55:15 +0530

    temp changes

--- exchange/bin/install.sh
+++ exchange/bin/install.sh
@@ -1,6 +1,6 @@
 
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 sudo apt update
